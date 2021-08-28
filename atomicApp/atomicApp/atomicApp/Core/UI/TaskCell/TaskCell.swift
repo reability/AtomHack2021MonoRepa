3fe2d1b8c5d24b4b955359eba3bbe9626c9a023b
reability <4reability@gmail.com>
2021-08-28 20:51:02 +0300

    ios

---
+++ atomicApp/atomicApp/atomicApp/Core/UI/TaskCell/TaskCell.swift
@@ -0,0 +1,22 @@
+//
+//  TaskCell.swift
+//  atomicApp
+//
+//  Created by Ванурин Алексей Максимович on 28.08.2021.
+//
+
+import SwiftUI
+
+struct TaskCell: View {
+    var body: some View {
+        Text("1 сентября")
+        Text("Обеспечить комлпексное развитие мер развития городской инфраструктуры на основе единых подходов")
+        Text("Департамент стройки")
+    }
+}
+
+struct TaskCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskCell()
+    }
+}
